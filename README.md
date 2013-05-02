67a48cf75b457660da29bb34396a29433b1a6244
Pavel Machalek <pavel@TCC-PAVEL.local>
2013-05-02 16:54:20 -0700

    Add pyfits and memory file handling

--- README.md
+++ README.md
@@ -8,3 +8,6 @@ http://keplergo.arc.nasa.gov/PyKE.shtml
 
 More on Python Map-Reduce on:
 http://www.michael-noll.com/tutorials/writing-an-hadoop-mapreduce-program-in-python/
+
+More on setting up Python on the console:
+http://osxastrotricks.wordpress.com/2012/05/02/python-setup-with-homebrew/
--- python/download.py
+++ python/download.py
@@ -2,10 +2,12 @@
 """An initial stab at a Kepler FITS downloader:
    http://www.michael-noll.com/tutorials/writing-an-hadoop-mapreduce-program-in-python/"""
 import sys
+import os
 import logging
 import urllib2
-import StringIO
-
+import tempfile
+from contextlib import contextmanager
+import pyfits
 
 QUARTER_PREFIXES = {'0':  "2010265121752",
                     '1':  "2009166043257",
@@ -29,31 +31,63 @@ def read_input(file):
         # split the line into words
         yield line.split()
 
+@contextmanager
+def tempinput(data):
+    """
+    Handle old legacy code that absolutely demands a filename
+    instead of streaming file content.
+    """
+    temp = tempfile.NamedTemporaryFile(delete=False)
+    temp.write(data)
+    temp.close()
+    yield temp.name
+    os.unlink(temp.name)
+
+def process_fits_object(fits_string):
+    """
+    Process FITS file object and extract info.
+    http://stackoverflow.com/questions/11892623/python-stringio-and-compatibility-with-with-statement-context-manager
+    """
+    
+    with tempinput(fits_string) as tempfilename:
+        pyfits.info(tempfilename)
+    return
+
 def download_file_serialize(uri, kepler_id, quarter):
-    "Download FITS file for each quarter for each object into memory and read FITS file"
+    """"
+    Download FITS file for each quarter for each object into memory and read FITS file.
+    """
     logging.warning("Downloading: KIC"+kepler_id+" quarter: "+quarter)
-    fits_object = StringIO.StringIO()
     try:
         response = urllib2.urlopen(uri)
         fits_stream = response.read()
-        #Write file object
-        fits_object.write(fits_stream)
+        #Write file object (but do not save to a local file)
     except:
         logging.error("Cannot download: "+ uri)
-        fits_object.write("")
-    return fits_object
+        fits_stream = ""
+    return fits_stream
 
 def prepare_path(kepler_id,quarter):
+    "Construct download path frm MAST."
     prefix = kepler_id[0:4]
     path = "http://archive.stsci.edu/pub/kepler/lightcurves/"+\
         prefix+"/"+kepler_id+"/kplr"+kepler_id+"-"+QUARTER_PREFIXES[quarter]+"_llc.fits"
     return path
 
 def main(separator="\t"):
+    """"
+    Read from KICs and quarters from STDIN, download FITS files and
+    process FITS file on the lfy in memory.
+    """
     data = read_input(sys.stdin)
     for kepler_id, quarter in data:
         path = prepare_path(kepler_id, quarter)
         fits_stream = download_file_serialize(path, kepler_id, quarter)
+        
+        status = process_fits_object(fits_stream)
+
+        #Write the result to STDOUT as this will be an input to a
+        #reducer that aggregates the querters together
         print kepler_id, quarter, path
 
 if __name__ == "__main__":
