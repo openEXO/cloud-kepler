4b595134fef48fa6e580578d19f20e2e202982b7
Pavel Machalek <pavel@TCC-PAVEL.local>
2013-05-02 10:28:51 -0700

    first commit
    - Add python downloader

---
+++ README.md
@@ -0,0 +1,9 @@
+Prototype of Cloud Kepler - pipeline for processing Kepler Sapce
+Telescope time series and search for planets. 
+
+More on the project on:
+http://kepler.nasa.gov/
+http://keplergo.arc.nasa.gov/PyKE.shtml
+
+More on Python Map-Reduce on:
+http://www.michael-noll.com/tutorials/writing-an-hadoop-mapreduce-program-in-python/
---
+++ python/download.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+"""An initial stab at a Kepler FITS downloader:
+   http://www.michael-noll.com/tutorials/writing-an-hadoop-mapreduce-program-in-python/"""
+import sys
+import argparse
+
+QUARTER_PREFIXES = {0: "something"}
+
+def read_input(file):
+    for line in file:
+        # split the line into words
+        yield line.split()
+
+def main(separator="\t"):
+    data = read_input(sys.stdin)
+    for kepler_id, quarter in data:
+        prefix = kepler_id[0:3]
+        path = "http://archive.stsci.edu/pub/kepler/lightcurves/"+\
+            prefix+"/"+kepler_id+"/kplr"+kepler_id+"-"+QUARTER_PREFIXES[quarter]+"_llc.fits"
+        print kepler_id, quarter, path
+
+if __name__ == "__main__":
+    main()
+
+        
