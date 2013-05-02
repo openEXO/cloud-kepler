ddd5bfac0b67470a631c3ff507c0d1975ed7cb66
Pavel Machalek <pavel@TCC-PAVEL.local>
2013-05-02 14:35:49 -0700

    Add quarter descriptors

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
-Prototype of Cloud Kepler - pipeline for processing Kepler Sapce
-Telescope time series and search for planets. 
+Prototype of Cloud Kepler - pipeline for processing Kepler Space
+Telescope time series and search for planets.
 
 More on the project on:
 http://kepler.nasa.gov/
+
 http://keplergo.arc.nasa.gov/PyKE.shtml
 
 More on Python Map-Reduce on:
--- python/download.py
+++ python/download.py
@@ -3,22 +3,40 @@
    http://www.michael-noll.com/tutorials/writing-an-hadoop-mapreduce-program-in-python/"""
 import sys
 import argparse
+import urllib2
 
-QUARTER_PREFIXES = {'0': "something",
-                    '1': "something",
-                    '2': "something",}
+QUARTER_PREFIXES = {'0':  "2010265121752",
+                    '1':  "2009166043257",
+                    '2':  "2009259160929",
+                    '3':  "2009350155506",
+                    '4':  "2010078095331",
+                    '5':  "2010174085026",
+                    '6':  "2010265121752",
+                    '7':  "2010355172524",
+                    '8':  "2011073133259",
+                    '9':  "2011177032512",
+                    '10': "2011271113734",
+                    '11': "2012004120508",
+                    '12': "2012088054726",
+                    '13': "2012179063303",
+                    '14': "2012277125453",
+                    '15': "2013011073258",}
 
 def read_input(file):
     for line in file:
         # split the line into words
         yield line.split()
 
+def prepare_path(kepler_id,quarter):
+    prefix = kepler_id[0:3]
+    path = "http://archive.stsci.edu/pub/kepler/lightcurves/"+\
+        prefix+"/"+kepler_id+"/kplr"+kepler_id+"-"+QUARTER_PREFIXES[quarter]+"_llc.fits"
+    yield path
+
 def main(separator="\t"):
     data = read_input(sys.stdin)
     for kepler_id, quarter in data:
-        prefix = kepler_id[0:3]
-        path = "http://archive.stsci.edu/pub/kepler/lightcurves/"+\
-            prefix+"/"+kepler_id+"/kplr"+kepler_id+"-"+QUARTER_PREFIXES[quarter]+"_llc.fits"
+        path = prepare_path(kepler_id, quarter)
         print kepler_id, quarter, path
 
 if __name__ == "__main__":
