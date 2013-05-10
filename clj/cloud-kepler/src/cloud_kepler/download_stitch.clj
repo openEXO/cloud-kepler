6958efbdb752fcd595e1b0d69b056dcc8de545d0
Pavel Machalek <pavel@TCC-PAVEL.local>
2013-05-09 20:16:35 -0700

    Flash out mappers and reducers

--- clj/cloud-kepler/src/cloud_kepler/download_stitch.clj
+++ clj/cloud-kepler/src/cloud_kepler/download_stitch.clj
@@ -1,15 +1,53 @@
 (ns cloud-kepler.download-stitch
   (:use
    cascalog.api
+   [cascalog
+    conf
+    [tap :as tap]
+    [workflow :as w]
+    [io: as io]
+    [api :as api]]
    [clojure.tools.cli :only (cli)]
    [clojure.string :only (split join)])
   (:import
    [cascading.flow MapReduceFlow]
+   [cascading.scheme Scheme TextDelimited TextLine
+    TextLine$Compress]
    [cascading.cascade CascadeConnector]
+   [org.apache.hadoop.streaming StreamJob]
+   [org.apache.hadoop.conf Configuration]
    [org.apache.hadoop.fs
     FileStatus FileSystem LocalFileSystem
     FileUtil Path])
   (:gen-class))
+
+(defn download-stitch-cascade
+  "Creata a download and stitch cascade for the first part of the
+   cloud-kepler pipeline."
+  [kepler-ids-quarters joined-quarters ;taps
+   python jar] ;options
+  (let [input (.toString (.getPath (kepler-ids-quarters :source)))
+        output (.toString (.getPath (joined-quarters :sink)))
+        python-anchor (if jar (last (split jar #"#")) ".")
+        map-script (join "/" [python-anchor "python/download.py"])
+        mapper (join " " [python map-script])
+        reduce-script (join "/" [python-anchor
+                                 "python/join_quarters.py"])
+        reducer (join " " [python reduce-script])
+        job-configuration (StreamJob/createJob
+                           (into-array
+                            (concat ["-input"  input
+                                     "-output" output
+                                     "-mapper" mapper
+                                     "-reducer" reducer]
+                                    (when jar ["-cacheArchive" jar]))))
+        flow (MapReduceFlow. "download-stitch" job-configuration)]
+    (.connect (CascadeConnector.) (into-array MapReduceFlow [flow]))))
+
+(defn format-tab-tap
+  "Create text delimited Cascading tab."
+  [path opts]
+  (tap/hfs-tap TextDelimited path opts))
 
 (defn -main
   "Download Kepler FITS files, decode and stitch quarters together."
@@ -17,11 +55,17 @@
   (let [[opts remaining help]
         (cli args
              ["--python" "Path to python interpreter"]
+             ["--jar" "Path to python jar"]
              )]
     (let [[input-path output-path] remaining
-          input-tap (format-tab-tap input-tap)
-          output-tap (format-tab-tap output-tap)
+          input-tap (format-tab-tap input-path
+                                    :fields [["kic_id" String
+                                              "quarter" String]])
+          output-tap (format-tab-tap output-path
+                                     :fields [["kic_id" String]
+                                              ["all_quarters" String]
+                                              ["flux" String]])
           download-stitch-cascade (download-stitch-q
                                    input-tap output-tap
-                                   (opts :python))
+                                   (opts :python) (opts :jar))
           (.complete download-stitch-cascade)])))
