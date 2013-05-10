8e46914fc5e00be12d8f9bb4ab8c1858054e2fde
Pavel Machalek <pavel@TCC-PAVEL.local>
2013-05-09 20:40:29 -0700

    Add jobdef

---
+++ clj/cloud-kepler/src/cloud_kepler/jobdefs/cloud-kepler-jobdef.clj
@@ -0,0 +1,33 @@
+;;;;Run cloud-kepler pipeline
+
+(comment
+  ***
+  Welcome to the cloud-kepler pipeline. Find your Earth-sized planet using the Amazon cloud. 
+  ***)
+
+(catch-args
+  [:python "python interpreter" nil]
+  [:kid-input "Kepler id, quarter input file" nil])
+
+(deflcuster cloud-kepler-cluster
+  :num-instances 2
+  :spot-task-group "c1.xlarge, 75%, 10"
+  :app "cloud-kepler"
+  :slave-instance-type "c1.xlarge"
+  :master-instance-type "m2.2xlarge"
+  :hadoop-config.custom ["-m" "mapred.tasktracker.map.tasks.maximum=8"])
+
+(defstep download-stitch
+  :main-class "cloud-kepler.download-stitch"
+  :step-name "Downoload FITS files and stitch quarters"
+  :remote-kic-file "${data-uri}/kic-input.txt"
+  :upload ["${kid-input}" :to "${remote-kic-file}"]
+  :output-path "${data-uri}/joined-quarters"
+  :local {:hadoop-env {"HADOOP_HEAPSIZE" "2048"}
+          :python-jar nil
+          :num-map-tasks "1"}
+  :args.python "${python-interpreter}"
+  :args.jar "${python-jar}"
+  :args.positional ["${remote-kic-file}" "${output-path}"])
+
+(fire! cloud-kepler-cluster download-stitch)
