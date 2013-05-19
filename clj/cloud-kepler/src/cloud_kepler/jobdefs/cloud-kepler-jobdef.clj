c1e46ed7b8d0938e2e712819da5e8a26e122f3ad
Pavel Machalek <pavel@TCC-PAVEL.local>
2013-05-19 16:49:14 -0700

    Jobdef for BLS

--- clj/cloud-kepler/src/cloud_kepler/jobdefs/cloud-kepler-jobdef.clj
+++ clj/cloud-kepler/src/cloud_kepler/jobdefs/cloud-kepler-jobdef.clj
@@ -2,17 +2,25 @@
 
 (comment
   ***
-  Welcome to the cloud-kepler pipeline. Find your Earth-sized planet using the Amazon cloud. 
+  Welcome to the cloud-kepler pipeline. Find your Earth-sized planet using the Amazon cloud.
   ***)
 
 (catch-args
   [:python "python interpreter" nil]
   ;http://stackoverflow.com/questions/1252965/distributing-my-python-scripts-as-jars-with-jython
   [:python-jar "Path to the python jar" nil]
-  [:kid-input "Kepler id, quarter input file" nil])
+  [:kid-input "Kepler id, quarter input file" nil]
+  [:min-per "Minimum planetary ortbital period" 1.5]
+  [:max-per "Maximum planetrary orbital period" 100]
+  [:min-dur "Minimum duration of transit in hours" 1.5]
+  [:max-dur "Maximum duration of transit in hours" 15]
+  [:nsearch "Number of trial periods" 100]
+  [:nbins "Number of phase bins" 100])
 
 (add-validators
-  (val-opts :required [:python :kid-input :python-jar]))
+  (val-opts :required [:python :kid-input :python-jar
+                       :minper :maxper :mindur :maxdur
+                       :nsearch :nbins]))
 
 (defcluster cloud-kepler-cluster
   :num-instances 2
@@ -24,7 +32,7 @@
           :num-map-tasks "1"
           :python "python"
           :kid-input "test/test_q1.txt"}
-    
+
   :app "cloud-kepler"
   :keypair "only_local"
   :slave-instance-type "c1.xlarge"
@@ -36,9 +44,23 @@
   :step-name "Downoload FITS files and stitch quarters"
   :remote-kic-file "${data-uri}/kic-input.txt"
   :upload ["${kid-input}" :to "${remote-kic-file}"]
-  :output-path "${data-uri}/joined-quarters"
+  :stitched-path "${data-uri}/joined-quarters"
   :args.python "${python}"
   :args.jar "${python-jar}"
-  :args.positional ["${remote-kic-file}" "${output-path}"])
+  :args.positional ["${remote-kic-file}" "${stitched-path}"])
+
+(defstep bls-search
+  :main-class "cloud_kepler.hadoop.bls"
+  :step-name "Perform Box Least Square period finding"
+  :bls-output-path "${data-uri}/bls-output"
+  :args.python "${python}"
+  :args.jar "${python-jar}"
+  :args.minper "${minper}"
+  :args.maxper "${maxper}"
+  :args.mindur "${mindur}"
+  :args.maxdur "${maxdur}"
+  :args.nsearch "${nsearch}"
+  :args.nbins "${nbins}"
+  :args.positional ["${stitched-path}" "${bls-output-path}"])
 
 (fire! cloud-kepler-cluster download-stitch)
