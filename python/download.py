1cc1e38afe4ef71ed461349a470e6a87783da826
Jack Flowers <flowersj@reed.edu>
2013-06-27 13:54:38 -0400

    fixes a glitch in windows by threading temp file name through process_fits_object into main, so that os.unlink can be called there

--- python/download.py
+++ python/download.py
@@ -45,22 +45,26 @@ def tempinput(data):
     temp.write(data)
     temp.close()
     yield temp.name
-    os.unlink(temp.name)
 
 def process_fits_object(fits_string):
     """
     Process FITS file object and extract info.
     http://stackoverflow.com/questions/11892623/python-stringio-and-compatibility-with-with-statement-context-manager
     """
-
+    test = ""
     with tempinput(fits_string) as tempfilename:
+        test = tempfilename
         fits_list = pyfits.getdata(tempfilename)
         error_status = np.asarray([c[9] for c in fits_list])
         time_pdcflux_pdcerror = np.asarray(
             [[c[0],c[7],c[8]] for i,c in
              enumerate(fits_list) if error_status[i] == 0])
-    return base64.b64encode(compress(simplejson.dumps(time_pdcflux_pdcerror.tolist())))
-
+        retval = base64.b64encode(compress(simplejson.dumps(time_pdcflux_pdcerror.tolist())))
+        #fix for windows, returns the filename into main so that os.unlink can be called there
+        #props go to the swag STScI IT guy for figuring this out.
+        #I'll add his name here once I work up the courage to ask him.
+        return test, retval
+        
 def download_file_serialize(uri, kepler_id, quarter):
     """"
     Download FITS file for each quarter for each object into memory and read FITS file.
@@ -91,11 +95,12 @@ def main(separator="\t"):
         try:
             path = prepare_path(kepler_id, quarter)
             fits_stream = download_file_serialize(path, kepler_id, quarter)
-            fits_array_string = process_fits_object(fits_stream)
+            tempfile, fits_array_string = process_fits_object(fits_stream)
 
             #Write the result to STDOUT as this will be an input to a
             #reducer that aggregates the querters together
             print "\t".join([kepler_id, quarter, path, fits_array_string])
+            os.unlink(tempfile)
         except:
             pass
 
