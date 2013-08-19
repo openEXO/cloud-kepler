dda2bc3358aa6f45b091253b0a52d60636a95206
Jack Flowers <flowersj@reed.edu>
2013-08-19 00:07:33 -0400

    updated my bls code to search a range of evenly spaced frequencies instead of periods. Recent testing seems to show that it works now.

--- python/peter_bls.py
+++ python/peter_bls.py
@@ -8,7 +8,7 @@ import math
 
 logger = logging.getLogger(__name__)
 logger.setLevel(logging.DEBUG)
-#This code is incomplete and still being tested. So don't try to use it for anything serious.
+#I think it actually works now and is faster than the other bls code, not as many bells and whistles though. Outputs are not properly formatted yet.
 def read_mapper_output(file, separator='\t'):
 #reads data
     for line in file:
@@ -36,7 +36,7 @@ def main():
     nbins = int(opts.nbins)
     mindur = max(int(qmin * nbins),1)
     maxdur = int(qmax * nbins) + 1
-    trialPeriods = numpy.linspace(minper,maxper,np)
+    trialFreqs = numpy.linspace(1.0/minper,1.0/maxper,np)
     input = read_mapper_output(sys.stdin)
     for kic, q, data in input:
 #format data arrays
@@ -57,18 +57,18 @@ def main():
         transitDuration = numpy.array([])
         transitPhase = numpy.array([])
         srMax = numpy.array([])
-        for trial in trialPeriods:
+        for trial in trialFreqs:
 #period iteration
 	#make sure bins not greater than data points in period
             nbins = int(opts.nbins)
-            if trial / .020433 + 1 < nbins:
-                nbins = int(trial / .020433 + 1)
+            if 1 /(trial * .020833) + 1 < nbins:
+                nbins = int(1 / (trial * .020833) + 1)
                 mindur = max(int(qmin * nbins),1)
                 maxdur = int(qmax * nbins) + 1
 	#bin data points
             ppb = numpy.zeros(nbins)
             binFlx = numpy.zeros(nbins)
-            phase = timeSet/trial - numpy.floor(timeSet/trial)
+            phase = timeSet*trial - numpy.floor(timeSet*trial)
             bin = numpy.floor(phase * nbins)
             for x in xrange(n):
                 ppb[int(bin[x])] += 1
@@ -94,7 +94,7 @@ def main():
         srMax = abs(srMax)**.5
         bestSr = numpy.max(srMax)
         t = numpy.nonzero(srMax == bestSr)
-        print "\t".join(map(str,[kic, bestSr, trialPeriods[t], transitPhase[t], transitDuration[t]]))
+        print "\t".join(map(str,[kic, bestSr, trialFreqs[t], transitPhase[t], transitDuration[t], srMax]))
 		
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO)
