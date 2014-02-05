f5dd4363a0bee7553cf9222c45efa43d58d4a62b
Scott Fleming <fleming@stsci.edu>
2014-02-05 16:43:06 -0500

    - Intermediate check-in of unit test for bls_pulse{_vec}.

--- python/sim_lc/bls_vec_simulator.py
+++ python/sim_lc/bls_vec_simulator.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
 
-def simulate_box_lightcurve(period=5, transit_ratio=0.025, transit_depth=0.010, phase=0.3, signal_to_noise=1, n_samples=3000, time_span=60):
+def bls_vec_simulator(period=5, transit_ratio=0.025, transit_depth=0.010, phase=0.3, signal_to_noise=1, n_samples=3000, time_span=60):
     """Simulate square-box light curve
 
     See Kovacs, 2002
@@ -14,6 +14,8 @@ def simulate_box_lightcurve(period=5, transit_ratio=0.025, transit_depth=0.010, 
         Ratio of transit duration to period (q)
     transit_depth : float
         Depth of the transit [mag]
+    phase : float
+        Orbital phase corresponding to the *start* of the transit (specify between 0. and 1.)
     signal_to_noise : float
         Ratio of transit_depth to white noise standard deviation
     n_samples : integer
@@ -23,8 +25,11 @@ def simulate_box_lightcurve(period=5, transit_ratio=0.025, transit_depth=0.010, 
 
     Returns
     =======
-    flux : array
-        Square-box simulated flux with white noise
+    A Dictionary that contains the following keys and data structures:
+       lc : light_curve [DataFrame]: A pandas DataFrame consisting of ndarrays where the first column is the flux, the second column is the flux error, and the index is the timestamps of each data point.
+       transit_times: transittimes [list]: A List of transit mid-point times, used for comparing results from other programs to see which transits were found.
+       transit_depths: depths [list]: A List of transit depths, used for comparing results from other programs to see which transits were found.
+       transit_durations: durations [list]: A List of transit durations (in hours), used for comparing results from other programs to see which transits were found.
     """
 
     time = pd.Index(np.linspace(0, time_span, num=n_samples), name="Time [d]")
@@ -34,10 +39,11 @@ def simulate_box_lightcurve(period=5, transit_ratio=0.025, transit_depth=0.010, 
     # initialize flux to 0
     light_curve = pd.DataFrame({
                                    "flux":       0, 
-                                   "flux_error": white_noise_std,
+                                   "flux_error": white_noise_std
                                }, index=time)
 
     # include transit
+    ## NOTE (SWF): There is a bug here because if the startings phase of the transit is close to 1.0 and the duration pushes it over, it will not wrap properly as written.
     light_curve.flux[(transit_phase > phase) & (transit_phase < (phase + transit_ratio))] -= transit_depth
 
     # remove mean
@@ -46,4 +52,18 @@ def simulate_box_lightcurve(period=5, transit_ratio=0.025, transit_depth=0.010, 
     # add white noise
     light_curve.flux += np.random.normal(loc=0, scale=white_noise_std, size=n_samples)
 
-    return light_curve
+    # create array of transit midpoint times, depths, and durations for passing back to calling program.  used so the calling program can compare results to the (known) transit parameters created here.
+    # first, calculate the precise *phase* of central transit
+    phase_t0 = (phase+transit_ratio - phase) / 2. + phase
+    
+    
+    transittimes = []
+    depths = []
+    durations = []
+
+    return {'lc':light_curve, 'transit_times':transittimes, 'transit_depths':depths, 'transit_durations':durations}
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    logger.setLevel(logging.INFO)
+    bls_vec_simulator()
---
+++ python/sim_lc/test_bls_pulse.py
@@ -0,0 +1,85 @@
+############################################################################################
+## Place import commands and logging options.
+############################################################################################
+import logging
+import random
+import math
+import bls_vec_simulator
+
+import numpy
+import matplotlib.pyplot as matplot
+
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
+############################################################################################
+
+
+############################################################################################
+## This is the main routine.
+############################################################################################
+def main():
+
+    ## Generate repeatable, random tranist parameters selected from a uniform distribution for testing purposes.
+    
+    ## This is the value of 1 minute in days (at least roughly).
+    minute_in_days = 1. / (60. * 24.)
+
+    ## How many total lightcurves to simulate?
+    n_lcs = 10
+
+    ## How long of a baseline do you want to use (in days)?
+    baseline = 90.
+
+    ## Determine the number of samples.  We want 1 min. cadence, so the number of samples is the baseline in *minutes* such that each sample is 1 minute in duration.
+    n_samples = math.ceil(baseline / minute_in_days)
+
+    ## What signal-to-noise should the lightcurves have (we want these nearly perfect for unit testing, so use a high value).
+    signal_to_noise = 10000.
+
+    ## At what phase should we start the transits?  Let's randomize this too between 0. and 1.
+    phase_range = (0.0,1.0)
+
+    ## What is the period range?  These are in days.
+    per_range = (0.5, 30.)
+
+    ## What is the depth range?  These are in percentages.
+    depth_range = (0.01, 0.5)
+
+    ## What is the duration range?  These are in hours.
+    duration_range = (1., 5.)
+    
+    ## Start the random seed variable so the random sampling is repeatable.
+    random.seed(4)
+    
+    ## Create random distribution of periods, depths, and durations.
+    period_list = [random.uniform(per_range[0], per_range[1]) for x in range(n_lcs)]
+    depth_list = [random.uniform(depth_range[0], depth_range[1]) for x in range(n_lcs)]
+    duration_list = [random.uniform(duration_range[0], duration_range[1]) for x in range(n_lcs)]
+    phase_list = [random.uniform(phase_range[0], phase_range[1]) for x in range(n_lcs)]
+
+    ## Convert the duration (in hours) into transit duration ratios for use in "simulate_box_lightcurve".
+    ## ratio = duration (hours) / 24. * period (days)
+    duration_ratio_list = [x/(24.*y) for x,y in zip(duration_list, period_list)]
+
+    ## Andrea Zonca's "simulate_box_lightcurve" takes in the following parameters:
+    ##  period (days)
+    ##  transit duration (as a ratio of the transit duration to the orbital period)
+    ##  transit depth (in normalized units)
+    ##  phase shift (we will use 0.0 for now for simplicity)
+    ##  signal-to-noise (ratio of transit depth to standard deviation of the white noise)
+    ##  n_samples (the number of fixed time samples during the time span)
+    ##  time_span (the total time baseline of the simulated lightcurve, in days)
+
+    ## Create the simulated lightcurves.
+    for p, d, dr, ph in zip(period_list, depth_list, duration_ratio_list, phase_list):
+        this_lc = bls_vec_simulator.bls_vec_simulator(p, dr, d, ph, signal_to_noise, n_samples, baseline)
+        matplot.plot(numpy.asarray(this_lc['lc'].index), numpy.asarray(this_lc['lc'].flux))
+        matplot.show()
+
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    logger.setLevel(logging.INFO)
+    main()
+############################################################################################
