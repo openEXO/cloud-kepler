77faa3a590299af47899e28dd6c1e375bfd506e9
Andrea Zonca <code@andreazonca.com>
2014-05-15 09:57:49 -0700

    Completed bls_pulse_vec scripting interface

--- python/bls_pulse_vec_interface.py
+++ python/bls_pulse_vec_interface.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import simplejson
+import json
 from zlib import decompress, compress
 import base64
 import logging
@@ -13,7 +13,7 @@ def read_mapper_output(file, separator='\t'):
     """Read stdin input"""
     for line in file:
         kic, quarters, flux_string = line.rstrip().split(separator)
-        light_curve = pd.DataFrame(simplejson.loads((decompress(base64.b64decode(flux_string)))), columns=["time", "flux", "flux_error"]).set_index("time")
+        light_curve = pd.DataFrame(json.loads((decompress(base64.b64decode(flux_string)))), columns=["time", "flux", "flux_error"]).set_index("time")
         yield kic, quarters, light_curve
 
 def check_input_options(parser,opts):
@@ -33,13 +33,14 @@ def setup_input_options(parser):
     parser.add_option("-m", "--mindur", action="store", type="float", dest="min_duration", default=0.0416667, help="[Optional] Minimum transit duration to search for (days).  Default = 0.0416667 (1 hour).")
     parser.add_option("-d", "--maxdur", action="store", type="float", dest="max_duration", default=12.0, help="[Optional] Maximum transit duration to search for (days).  Default = 0.5 (12 hours).")
     parser.add_option("-b", "--nbins", action="store", type="int", dest="n_bins", default=100, help="[Optional] Number of bins to divide the lightcurve into.  Default = 100.")
+    parser.add_option("-o", "--detrend-order", action="store", type="int", dest="detrend_order", default=0, help="[Optional] Order of the polynomial detrending using PyKE.  Default = 0 (no detrending)")
     parser.add_option("--direction", action="store", type="int", dest="direction", default=0, help="[Optional] Direction of box wave to look for.  1=blip (top-hat), -1=dip (drop), 0=both (most significant).  Default = 0")
     parser.add_option("--printformat", action="store", type="string", dest="print_format", default="encoded", help="[Optional] Format of strings printed to screen.  Options are 'encoded' (base-64 binary) or 'normal' (human-readable ASCII strings).  Default = 'encoded'.")
     parser.add_option("-v", "--verbose", action="store_true", dest="verbose", default=False, help="[Optional] Turn on verbose messages/logging.  Default = False.")
 
 def encode_arr(arr):
     """64bit encodes numpy arrays"""
-    return base64.b64encode(compress(simplejson.dumps(arr.tolist())))
+    return base64.b64encode(compress(json.dumps(arr.tolist())))
 
 def main():
     parser = OptionParser(usage="%prog -p [-m] [-d] [-b] [--direction]", version="%prog 1.0")
@@ -60,17 +61,9 @@ def main():
     output = OrderedDict()
     for kic_id, quarters, light_curve in input_data:
         id_string = kic_id + quarters.replace(",","_").translate(None, "[]' ")
-        output[id_string] = bls_pulse_vec(light_curve, opts.segment, opts.min_duration, opts.max_duration, n_bins)
+        output[id_string] = bls_pulse_vec(light_curve, opts.segment, opts.min_duration, opts.max_duration, opts.n_bins, detrend_order=opts.detrend_order)
 
-        print "-" * 80
-        print "Kepler " + kic_id
-        print "Quarters: " + quarters
-        print "-" * 80
-        print "\t".join(map(str,['Segment','srMax', 'transitPhase', 'transitDuration', 'transitDepth', 'transitMidTime']))
-        for ii,seq in enumerate(segments):
-            print "\t".join(map(str,[ii, srMax[ii], transitPhase[ii], transitDuration[ii], transitDepth[ii], transitMidTime[ii]]))
-        print "-" * 80
-        print "\n"
+        print output[id_string]
 
 if __name__ == "__main__":
     main()
