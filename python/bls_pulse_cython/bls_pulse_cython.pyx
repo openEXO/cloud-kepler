bda272195cad4eddc92fbc583cd02696dabb72f7
Ellen Price <eprice@caltech.edu>
2014-07-05 01:14:26 -0400

    Setting up a detrending module [ci skip]

--- python/bls_pulse_cython/bls_pulse_cython.pyx
+++ python/bls_pulse_cython/bls_pulse_cython.pyx
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from detrend.polyfit import polyfit
 from numpy.polynomial import polynomial as poly
 cimport numpy as np
 cimport cython
@@ -71,7 +72,7 @@ int nbins, double segsize, double mindur, double maxdur, int detrend_order=3, di
             continue
 
         if detrend_order != 0:
-            coeffs = __lsqclip_detrend(stime[ndx], sflux[ndx], sfluxerr[ndx], detrend_order)
+            coeffs = polyfit(stime[ndx], sflux[ndx], sfluxerr[ndx], detrend_order)
             sflux /= poly.polyval(stime, coeffs)
 
         # Call the algorithm.
@@ -97,45 +98,6 @@ np.ndarray[double, ndim=1, mode='c'] flux, np.ndarray[double, ndim=1, mode='c'] 
     
     # The phase-binning assumes a zero-based time index.
     time -= time[0]
-
-
-@cython.boundscheck(False)
-@cython.wraparound(False)
-@cython.profile(True)
-def __lsqclip_detrend(np.ndarray[double, ndim=1, mode='c'] time, 
-np.ndarray[double, ndim=1, mode='c'] flux, np.ndarray[double, ndim=1, mode='c'] fluxerr, 
-int order, double threshold=3., int niter=3):
-    '''
-
-    '''
-    cdef int i
-    cdef double sigma
-    cdef np.ndarray[long, ndim=1, mode='c'] ndx
-    cdef np.ndarray[double, ndim=1, mode='c'] x, y, yerr, coeffs, resid
-
-    x = time.copy()
-    y = flux.copy()
-    yerr = fluxerr.copy()
-    i = 0
-    
-    while i < niter and len(x) > order + 1:
-        # Fit the current data or residuals with a polynomial.
-        coeffs = poly.polyfit(x, y, order, w=yerr)
-        resid = y - poly.polyval(x, coeffs)
-
-        # Calculate the root-mean-square statistic for the fit.
-        sigma = np.sqrt(np.sum(resid**2.) / len(x))
-
-        # Perform the sigma clipping.
-        ndx = np.where(np.absolute(resid) < threshold * sigma)[0]
-        x = x[ndx]
-        y = y[ndx]
-        yerr = yerr[ndx]
-        
-        # Increase the iteration counter.
-        i += 1
-
-    return coeffs
 
 
 @cython.boundscheck(False)
---
+++ python/detrend/polyfit/polyfit.pyx
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+from numpy.polynomial import polynomial as poly
+cimport numpy as np
+cimport cython
+
+
+@cython.boundscheck(False)
+@cython.wraparound(False)
+@cython.profile(True)
+def polyfit(np.ndarray[double, ndim=1, mode='c'] time,
+np.ndarray[double, ndim=1, mode='c'] flux, np.ndarray[double, ndim=1, mode='c'] fluxerr,
+int order, double threshold=3., int niter=3):
+    '''
+
+    '''
+    cdef int i
+    cdef double sigma
+    cdef np.ndarray[long, ndim=1, mode='c'] ndx
+    cdef np.ndarray[double, ndim=1, mode='c'] x, y, yerr, coeffs, resid
+
+    x = time.copy()
+    y = flux.copy()
+    yerr = fluxerr.copy()
+    i = 0
+
+    while i < niter and len(x) > order + 1:
+        # Fit the current data or residuals with a polynomial.
+        coeffs = poly.polyfit(x, y, order, w=yerr)
+        resid = y - poly.polyval(x, coeffs)
+
+        # Calcualte the root-mean-square statistic for the fit.
+        sigma = np.sqrt(np.sum(resid**2.) / len(x))
+
+        # Perform the sigma clipping.
+        ndx = np.where(np.absolute(resid) < threshold * sigma)[0]
+        x = x[ndx]
+        y = y[ndx]
+        yerr = yerr[ndx]
+
+        # Increase the iteration counter.
+        i += 1
+
+    return coeffs
+
