f1040c0a07ff82fe650c63626e4948a1e62786b7
Ellen Price <eprice@caltech.edu>
2014-08-11 16:12:35 -0400

    Trying to fix some documentation by mocking out Cython modules

--- docs/conf.py
+++ docs/conf.py
@@ -12,12 +12,17 @@
 # serve to show the default.
 
 import sys, os
+import mock
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../python'))
+
+MOCK_MODULES = ['bls_pulse_cython','detrend']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = mock.Mock()
 
 # -- General configuration -----------------------------------------------------
 
